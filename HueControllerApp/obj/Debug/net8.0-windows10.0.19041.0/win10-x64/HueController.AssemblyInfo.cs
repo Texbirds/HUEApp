7aa4db3090dbe8fa7eb2b91cf07cd2a5ead10e2b
Tex <kwintjanleenders@gmail.com>
2024-12-05 11:59:58 +0100

    Added language files

--- HueControllerApp/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/HueController.AssemblyInfo.cs
+++ HueControllerApp/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/HueController.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HueController")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8b9b31dae14416009bfa2bb78814553673e229cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c1e42213322332afbbe273cd91018ec82f4b1076")]
 [assembly: System.Reflection.AssemblyProductAttribute("HueController")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HueController")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
