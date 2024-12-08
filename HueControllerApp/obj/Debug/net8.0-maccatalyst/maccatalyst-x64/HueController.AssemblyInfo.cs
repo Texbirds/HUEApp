3f460f7aefa054208b8d715389c84707985da48b
Tex <kwintjanleenders@gmail.com>
2024-12-08 14:26:53 +0100

    changed xaml ID's for happy/unhappy tests

--- HueControllerApp/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/HueController.AssemblyInfo.cs
+++ HueControllerApp/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/HueController.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HueController")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e00a2cb80b93aa72e7b8c3d60324bef39480e000")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d716553a0edae4cde735b7c350f7b1321385a5d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HueController")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HueController")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
